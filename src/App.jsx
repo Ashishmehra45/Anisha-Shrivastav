a9a914ba671bdae9c3fc0e752fe896e58689a0b0
Ashish Mehra <ashish6266mehra@gmail.com>
2026-09-24 17:01:34 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -106,7 +106,7 @@ const App = () => {
       description:
         "Participated in the consultation on 'Atmanirbharata in Defence Manufacturing' at the MPIDC Conference Hall, Bhopal. The session convened senior government officials, industry representatives, and key stakeholders to explore opportunities for strengthening indigenous capabilities, fostering innovation, and accelerating the growth of India’s defence manufacturing sector.",
       images: [
-        "/press/AN-img1-Picsart-AiImageEnhancer.png",
+        "/bharatTex/258A0264.JPG",
         "/press/AN-img2.png",
         "/press/AN-img3.png",
       ],
